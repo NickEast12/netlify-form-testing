1f5df5356418d3d5855686e569a328c00ff73448
Nick <nickeast1998@gmail.com>
2020-11-17 15:52:49 +0000

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,50 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <form
+      className="contact__wrapper__inner__form"
+      name="contact"
+      method="post"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+      action="/success"
+    >
+      <input type="hidden" name="form-name" value="contact" />
+      <div className="contact__wrapper__inner__form__section">
+        <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          id="name"
+          required
+        ></input>
+        <input
+          type="text"
+          placeholder="Email"
+          name="email"
+          id="email"
+          required
+        ></input>
+      </div>
+      <input
+        type="text"
+        placeholder="Subject"
+        name="title"
+        id="title"
+        required
+      ></input>
+      <textarea
+        placeholder="Message"
+        name="message"
+        id="message"
+        required
+      ></textarea>
+      <div className="contact__wrapper__inner__form__btn">
+        <button type="submit">
+          <span>Submit</span>
+        </button>
+      </div>
+    </form>
   </Layout>
 )
 
